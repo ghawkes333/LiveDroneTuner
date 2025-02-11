cf0de801b54154f2e5c32003bb97d1b82bc2b14b
Gabriella Hawkes <ghawkes.dev@gmail.com>
2025-02-11 15:37:14 -0600

    basic

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,26 @@
 import { Text, View } from "react-native";
+import { PitchDetector } from 'react-native-pitch-detector';
 
+// async function test() {
+
+// // To start recording
+//   await PitchDetector.start(); // Promise<void>
+
+//   // // To stop recording
+//   // await PitchDetector.stop(); // Promise<void>
+
+//   // // To get current status
+//   // await PitchDetector.isRecording(); // Promise<true | false>
+
+//   // To listener results
+//   const subscription = PitchDetector.addListener(console.log) // { frequency: 440.14782, tone: "C#" }
+
+//   // // To stop listen results
+//   // PitchDetector.removeListener()
+// }
 
 export default function Index() {
+  PitchDetector.start()
   return (
     <View
       style={{
