5eddbea51650ecf2f26e04e8c7b03aad31be2d8d
Gabriella Hawkes <ghawkes.dev@gmail.com>
2025-02-21 09:46:04 -0600

    Initial Commit

---
+++ IOSLiveTuner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSLiveTuner
+//
+//  Created by Auburn University Student on 2/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOSLiveTunerTests/IOSLiveTunerTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOSLiveTunerTests.swift
+//  IOSLiveTunerTests
+//
+//  Created by Auburn University Student on 2/21/25.
+//
+
+import Testing
+@testable import IOSLiveTuner
+
+struct IOSLiveTunerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
