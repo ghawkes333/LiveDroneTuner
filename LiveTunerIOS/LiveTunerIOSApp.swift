f3c037f142d6d65ae6678c7bf2f50363de287455
Gabriella Hawkes <ghawkes.dev@gmail.com>
2025-02-20 20:26:01 -0600

    Blank XCode Project

---
+++ LiveTunerIOS/LiveTunerIOSApp.swift
@@ -0,0 +1,81 @@
+//
+//  LiveTunerIOSApp.swift
+//  LiveTunerIOS
+//
+//  Created by Auburn University Student on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveTunerIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
+}
+
+//func init() {
+//
+//    var status: OSStatus
+//
+//    do {
+//        try AVAudioSession.sharedInstance().setPreferredIOBufferDuration(preferredIOBufferDuration)
+//    } catch let error as NSError {
+//        print(error)
+//    }
+//
+//
+//    var desc: AudioComponentDescription = AudioComponentDescription()
+//    desc.componentType = kAudioUnitType_Output
+//    desc.componentSubType = kAudioUnitSubType_VoiceProcessingIO
+//    desc.componentFlags = 0
+//    desc.componentFlagsMask = 0
+//    desc.componentManufacturer = kAudioUnitManufacturer_Apple
+//
+//    let inputComponent: AudioComponent = AudioComponentFindNext(nil, &desc)
+//
+//    status = AudioComponentInstanceNew(inputComponent, &audioUnit)
+//    checkStatus(status)
+//
+//    var flag = UInt32(1)
+//    status = AudioUnitSetProperty(audioUnit, kAudioOutputUnitProperty_EnableIO, kAudioUnitScope_Input, kInputBus, &flag, UInt32(sizeof(UInt32)))
+//    checkStatus(status)
+//
+//    status = AudioUnitSetProperty(audioUnit, kAudioOutputUnitProperty_EnableIO, kAudioUnitScope_Output, kOutputBus, &flag, UInt32(sizeof(UInt32)))
+//    checkStatus(status)
+//
+//    var audioFormat: AudioStreamBasicDescription! = AudioStreamBasicDescription()
+//    audioFormat.mSampleRate = 8000
+//    audioFormat.mFormatID = kAudioFormatLinearPCM
+//    audioFormat.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked
+//    audioFormat.mFramesPerPacket = 1
+//    audioFormat.mChannelsPerFrame = 1
+//    audioFormat.mBitsPerChannel = 16
+//    audioFormat.mBytesPerPacket = 2
+//    audioFormat.mBytesPerFrame = 2
+//
+//    status = AudioUnitSetProperty(audioUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Output, kInputBus, &audioFormat, UInt32(sizeof(UInt32)))
+//    checkStatus(status)
+//
+//
+//    try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord)
+//    status = AudioUnitSetProperty(audioUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, kOutputBus, &audioFormat, UInt32(sizeof(UInt32)))
+//    checkStatus(status)
+//
+//
+//    // Set input/recording callback
+//    var inputCallbackStruct = AURenderCallbackStruct(inputProc: recordingCallback, inputProcRefCon: UnsafeMutablePointer(unsafeAddressOf(self)))
+//    AudioUnitSetProperty(audioUnit, AudioUnitPropertyID(kAudioOutputUnitProperty_SetInputCallback), AudioUnitScope(kAudioUnitScope_Global), 1, &inputCallbackStruct, UInt32(sizeof(AURenderCallbackStruct)))
+//
+//
+//    // Set output/renderar/playback callback
+//    var renderCallbackStruct = AURenderCallbackStruct(inputProc: playbackCallback, inputProcRefCon: UnsafeMutablePointer(unsafeAddressOf(self)))
+//    AudioUnitSetProperty(audioUnit, AudioUnitPropertyID(kAudioUnitProperty_SetRenderCallback), AudioUnitScope(kAudioUnitScope_Global), 0, &renderCallbackStruct, UInt32(sizeof(AURenderCallbackStruct)))
+//
+//
+//    flag = 0
+//    status = AudioUnitSetProperty(audioUnit, kAudioUnitProperty_ShouldAllocateBuffer, kAudioUnitScope_Output, kInputBus, &flag, UInt32(sizeof(UInt32)))
+//}
---
+++ LiveTunerIOSTests/LiveTunerIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTunerIOSTests.swift
+//  LiveTunerIOSTests
+//
+//  Created by Auburn University Student on 2/17/25.
+//
+
+import Testing
+@testable import LiveTunerIOS
+
+struct LiveTunerIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
