5ee223d4f0ec44210d04f79a07b9c1688e373289
Gabriella Hawkes <ghawkes.dev@gmail.com>
2025-03-03 06:49:01 -0600

    Show note name of mic, play audio on same speaker

--- IOSLiveTuner/ViewController.swift
+++ IOSLiveTuner/ViewController.swift
@@ -20,7 +20,8 @@ import CoreAudio
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var pitchLbl: UILabel!
     
     var avPlayer : AVAudioPlayer?
     var engine = AudioEngine()
@@ -32,7 +33,11 @@ class ViewController: UIViewController {
     let initialDevice: Device
     let silence: Fader
     
+    @Published var pitchLabelStr = "_"
+
+    
     required init?(coder decoder: NSCoder) {
+        
                 
         let path = Bundle.main.path(forResource: "theme.mp3", ofType: nil)!
         let url = URL(fileURLWithPath: path)
@@ -57,6 +62,9 @@ class ViewController: UIViewController {
         
         super.init(coder: decoder)
         
+//        pitchLbl.text = "Yay!"
+
+        
         do {
             avPlayer = try AVAudioPlayer(contentsOf: url)
             avPlayer?.numberOfLoops = -1
@@ -76,8 +84,46 @@ class ViewController: UIViewController {
             return
         }
         
-        tracker = PitchTap(mic) {pitch, amp in DispatchQueue.main.async {
-                    print(pitch[0])
+        tracker = PitchTap(mic) {p, a in DispatchQueue.main.async {
+            var pitch = p[0]
+            var amp = a[0]
+            let noteFrequencies = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
+            let noteNamesWithSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
+            let noteNamesWithFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
+            
+            guard amp > 0.1 else {print("Amp: ", amp); return}
+            
+            print("Success!")
+            var freq = pitch
+    
+            while freq > Float(noteFrequencies[noteFrequencies.count - 1]){
+                freq = freq / 2.0
+            }
+    
+            while freq < Float(noteFrequencies[0]){
+                freq = freq * 2.0
+            }
+            
+            var minDistance: Float = 10000.0
+    
+            var index = 0
+    
+            for j in 0 ..< noteFrequencies.count {
+                let distance = fabsf(Float(noteFrequencies[j]) - freq)
+                if distance < minDistance {
+                    index = j
+                    minDistance = distance
+                }
+            }
+            
+            let octave = Int(log2f(pitch / freq))
+    
+            var noteNameWithSharps = "\(noteNamesWithSharps[index])\(octave)"
+            var noteNameWithFlats = "\(noteNamesWithFlats[index])\(octave)"
+    
+            print(noteNameWithFlats)
+            self.pitchLabelStr = noteNameWithFlats
+            self.pitchLbl.text = noteNameWithFlats
         }}
         
         tracker.start()
@@ -106,6 +152,49 @@ class ViewController: UIViewController {
     
     @IBAction func checkPitches(){
         tracker.start()
+    }
+    
+    func freqToPitch(pitch : Float, amp : Float){
+        
+//        let noteFrequencies = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
+//        let noteNamesWithSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
+//        let noteNamesWithFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
+        
+//        guard amp > 0.1 else {print("Amp: ", amp); return}
+        
+//        print("Success!")
+//        var freq = pitch
+//        
+//        while freq > Float(noteFrequencies[noteFrequencies.count - 1]){
+//            freq = freq / 2.0
+//        }
+//        
+//        while freq < Float(noteFrequencies[0]){
+//            freq = freq * 2.0
+//        }
+        
+//        var minDistance: Float = 10000.0
+//        
+//        var index = 0
+//        
+//        for j in 0 ..< noteFrequencies.count {
+//            let distance = fabsf(Float(noteFrequencies[j]) - freq)
+//            if distance < minDistance {
+//                index = j
+//                minDistance = distance
+//            }
+//        }
+        
+//        let octave = Int(log2f(pitch / freq))
+//        
+//        var noteNameWithSharps = "\(noteNamesWithSharps[index])\(octave)"
+//        var noteNameWithFlats = "\(noteNamesWithFlats[index])\(octave)"
+//        
+//        print(noteNameWithFlats)
+//        pitchLbl.text = noteNameWithFlats
+        
+        
+        
     }
 
     
